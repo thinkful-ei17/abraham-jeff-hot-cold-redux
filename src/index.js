688aa3a90894425e1642b71a7eda0e3ba6ab48f5
Jeff Connelly <jeff.connelly6@gmail.com>
2018-01-29 11:33:27 -0700

    Initialized redux folders

--- src/index.js
+++ src/index.js
@@ -10,3 +10,13 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+
+/* Todo:
+
+1. Make the action creators
+2. Create a reducer to handle the actions
+3. Create the store
+***Dont touch the components yet***
+
+*/
