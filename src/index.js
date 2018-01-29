45a13051f2aad6989f3f9f8131f9253542de4d47
Jeff Connelly <jeff.connelly6@gmail.com>
2018-01-29 12:12:01 -0700

    Added redux dev tools and initialized files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './reset.css';
-import './index.css';
+import './index.css'; 
 
 import Game from './components/game';
 
@@ -10,13 +10,12 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
-
+  
 
 /* Todo:
-
 1. Make the action creators
 2. Create a reducer to handle the actions
 3. Create the store
-***Dont touch the components yet***
-
+4. Test it out by dispatching actions from src/index.js
+*** Dont touch the components yet ***
 */
